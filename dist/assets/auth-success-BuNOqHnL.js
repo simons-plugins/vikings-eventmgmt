091f0bd418eb2fcc40b166b6a71cea07db92f2b4
siclark <simon@starmerclark.com>
2025-06-23 22:48:58 +0100

    Remove unused Sentry monitoring files and HTML templates for authentication and main index page

--- dist/assets/auth-success-BuNOqHnL.js
+++
@@ -1,2 +0,0 @@
-(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},f=new e.Error().stack;f&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[f]="e3b0c442-98fc-4c14-9afb-f4c8996fb924",e._sentryDebugIdIdentifier="sentry-dbid-e3b0c442-98fc-4c14-9afb-f4c8996fb924")}catch{}})();
-//# sourceMappingURL=auth-success-BuNOqHnL.js.map
